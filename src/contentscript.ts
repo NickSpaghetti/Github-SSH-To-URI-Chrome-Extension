d60b87592ad19d56b401adebc4702af7c58a376b
Nicholas Cicchetti <nc_cicchetti@yahoo.com>
2022-11-21 11:57:26 -0500

    Added Tf Parser

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,9 +1,29 @@
+import { url } from "inspector";
+import { Url } from "url";
+
+const fs = require('fs');
+const parser = require('@evops/hcl-terraform-parser');
+
 enum FileType {
     tf = 'tf',
     hlc = 'hlc'
 }
 
-const getFileType = () => {
+interface IDisplayModule {
+    uri: URL;
+    source: string;
+}
+
+class DisplayModule implements IDisplayModule{
+    uri: URL =  new URL('')
+    source: string = ''
+    constructor(uri: string, source: string){
+        this.uri = new URL(uri);
+        this.source = source;
+    }
+}
+
+const getFileType = ():string => {
     const finalPath = document.querySelector('.final-path') as HTMLElement
     const innerText = finalPath?.innerText ?? '';
     const fileType = innerText.split('.');
@@ -15,13 +35,25 @@ const getFileType = () => {
 
 }
 
-const findModules = () => {
-    //regex string module (\"[\w]+\" \{)\n  source [\s]+\= (\"[\w\d\s].+\")
+const findModuleSources = ():IDisplayModule[] => {
+    const dataTargetElement = document.querySelector('.data-target') as HTMLElement
+    const innerText = dataTargetElement?.innerText ?? '';
+    if(innerText === ''){
+        return [];
+    } 
+
+    const hlcFile = parser.parse(innerText);
+    let sources: IDisplayModule[] = [];
+    for (let value of hlcFile.module_calls.values()){
+        sources.push(new DisplayModule(value.source,value.name))
+    }
+    
+    return sources;
 }
 
-const getSourceUri = () => {
+const getSourceUri = ():URL => {
     const uri = ""
-    return uri;
+    return new URL(uri);
 }
 
 chrome.runtime.onMessage.addListener((message, sender, callback) => {
@@ -30,6 +62,7 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
     if(!(fileType in FileType)){
         return;
     }
-    let sourceUri = getSourceUri();
+    let sources :string[] = findModuleSources();
+    let sourceUris :string[] = getSourceUri();
     callback(`uri: ${getSourceUri()}`);
 });
