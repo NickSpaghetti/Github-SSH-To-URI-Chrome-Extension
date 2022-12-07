625124bd060451624673bf072229775d0672545e
Nicholas Cicchetti <nc_cicchetti@yahoo.com>
2022-12-06 19:56:10 -0500

    retracked correct files

---
+++ src/popup-page/popup.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useEffect, useState} from 'react';
+import {render} from 'react-dom';
+
+interface IProps {
+  
+}
+
+export const Popup: FC<IProps> = () => {
+
+  const [content, setContent] = useState('N/A');
+
+  useEffect(() => {
+    chrome.tabs.query({currentWindow: true, active: true}, tabs => {
+      const currentTabID = tabs.length === 0 ? 0 : tabs[0].id!;
+      chrome.tabs.sendMessage(currentTabID, '', response => {
+        setContent(response);
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      {content}
+    </div>
+  );
+}
+
+render(<Popup />, document.getElementById("sources-popup"))
